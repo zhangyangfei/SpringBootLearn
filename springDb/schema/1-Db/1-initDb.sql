3d6c9d7f1f4d88c2f3849ec4a811c2c143f6f872
zhangyangfei <674557629@qq.com>
2019-09-22 15:06:18 +0800

    连接mysql

--- springDb/schema/1-Db/1-initDb.sql
+++ springDb/schema/1-Db/1-initDb.sql
@@ -6,3 +6,6 @@ CREATE USER 'springdb'@'%' IDENTIFIED BY 'springdb';
 
 /*授予所有的权限,但不能给其它用户授权*/
 GRANT ALL ON *.* TO 'springdb'@'%';
+
+/*设置时区（在mysql中设置时区，默认为SYSTEM）*/
+set global time_zone='+8:00';
--- springDb/src/test/java/com/zyf/springDb/SpringDbApplicationTests.java
+++ springDb/src/test/java/com/zyf/springDb/SpringDbApplicationTests.java
@@ -1,17 +1,26 @@
 package com.zyf.springDb;
 
+import java.util.List;
+import java.util.Map;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SpringDbApplicationTests {
 
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
 	@Test
 	public void contextLoads() {
 		System.out.println("hello springDb test");
+		List<Map<String, Object>> list = jdbcTemplate.queryForList("select * from t_user");
+		System.out.println(list);	
 	}
-
+	
 }
