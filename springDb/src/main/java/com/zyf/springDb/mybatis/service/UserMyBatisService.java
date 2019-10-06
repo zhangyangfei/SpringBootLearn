0f4079c890b3a94edbb7023397fb1bfd1905ab2a
zhangyangfei <674557629@qq.com>
2019-10-06 11:25:01 +0800

    隔离级别

--- springDb/src/main/java/com/zyf/springDb/mybatis/service/UserMyBatisService.java
+++ springDb/src/main/java/com/zyf/springDb/mybatis/service/UserMyBatisService.java
@@ -2,6 +2,8 @@ package com.zyf.springDb.mybatis.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.zyf.springDb.mybatis.dao.UserMapper;
 import com.zyf.springDb.mybatis.dto.UserDto;
@@ -11,8 +13,13 @@ public class UserMyBatisService {
 
 	@Autowired
 	private UserMapper userMapper;
-	
-	public UserDto getUserById(int id){
+
+//	@Transactional(isolation = Isolation.DEFAULT) // 默认
+//	@Transactional(isolation = Isolation.READ_COMMITTED) // 读写提交
+//	@Transactional(isolation = Isolation.READ_UNCOMMITTED) // 未读提价
+//	@Transactional(isolation = Isolation.REPEATABLE_READ) // 可重复读
+	@Transactional(isolation = Isolation.SERIALIZABLE) // 串行化
+	public UserDto getUserById(int id) {
 		return userMapper.getUserById(id);
 	}
 }
