b96f87dc2ecf3cdd781398f54ea4771baab5d517
zhangyangfei <674557629@qq.com>
2019-11-01 17:51:14 +0800

    mvc自定义参数校验

--- springMVC/src/main/java/com/zyf/springMVC/mvcparamvalidate/MvcParamValidateController.java
+++ springMVC/src/main/java/com/zyf/springMVC/mvcparamvalidate/MvcParamValidateController.java
@@ -8,6 +8,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.validation.Errors;
 import org.springframework.validation.FieldError;
 import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -22,12 +24,43 @@ public class MvcParamValidateController {
 	}
 
 	/**
-	 * JSR-303验证
+	 * JSR-303参数验证
 	 * 	注解@Valid开启验证，否则errors.getAllErrors()不能获取到错误信息
 	 */
 	@RequestMapping(value = "/mpv2")
 	@ResponseBody
 	private User mpv2(@Valid @RequestBody User user, Errors errors) {
+		List<ObjectError> errList = errors.getAllErrors();
+		for (ObjectError oError : errList) {
+			// 如果是字段错误
+			if (oError instanceof FieldError) {
+				FieldError fe = (FieldError) oError;
+				System.out.println(fe.getField() + "=" + fe.getRejectedValue() + "，消息=" + fe.getDefaultMessage());
+			} else {
+				System.out.println(oError.getObjectName() + "，消息=" + oError.getDefaultMessage());
+			}
+		}
+		return user;
+	}
+	
+	/**
+	 * 绑定自定义参数校验器
+	 * 	用setValidator()方法，原来的JSR-303则不做校验了
+	 * 	用addValidators()方法，原来的JSR-303校验依旧有效，且自定义校验也有效（推荐）
+	 */
+	@InitBinder
+	public void initBinder(WebDataBinder binder) {
+		// binder.setValidator(new UserValidator());//设定参数校验器
+		binder.addValidators(new UserValidator());// 增加参数校验器（可多个）
+	}
+	
+	/**
+	 * 自定义参数校验
+	 * 	注解@Valid开启验证，否则JSR-303验证和UserValidator()都不生效
+	 */
+	@RequestMapping(value = "/mpv3")
+	@ResponseBody
+	private User mpv3(@Valid @RequestBody User user, Errors errors) {
 		List<ObjectError> errList = errors.getAllErrors();
 		for (ObjectError oError : errList) {
 			// 如果是字段错误
---
+++ springMVC/src/main/java/com/zyf/springMVC/mvcparamvalidate/UserValidator.java
@@ -0,0 +1,28 @@
+package com.zyf.springMVC.mvcparamvalidate;
+
+import java.math.BigDecimal;
+
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+
+/** 自定义参数验证器 */
+public class UserValidator implements Validator {
+	
+	/** 声明支持的校验类型为User */
+	@Override
+	public boolean supports(Class<?> clazz) {
+		return clazz.equals(User.class);
+	}
+
+	/** 校验规则 */
+	@Override
+	public void validate(Object target, Errors errors) {
+		if (null != target) {
+			//supports()方法支持User，此处可以强转为User
+			User user = (User) target;
+			if (new BigDecimal("1000000").compareTo(user.getYue()) > 0) {
+				errors.rejectValue("yue", "", "余额不足，请充值！");
+			}
+		}
+	}
+}
