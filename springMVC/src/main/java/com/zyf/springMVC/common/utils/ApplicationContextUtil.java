03387d3b25997c72f61a3f3ba63dce634f06f322
zhangyangfei <674557629@qq.com>
2019-11-04 11:24:36 +0800

    注解

--- springMVC/src/main/java/com/zyf/springMVC/common/utils/ApplicationContextUtil.java
+++ springMVC/src/main/java/com/zyf/springMVC/common/utils/ApplicationContextUtil.java
@@ -16,24 +16,33 @@ public class ApplicationContextUtil implements ApplicationContextAware {
 
 	private static ApplicationContext applicationContext;
 
+	/** ApplicationContext对象初始化 */
 	@Override
 	public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
 		ApplicationContextUtil.applicationContext = applicationContext;
 	}
 
+	/** 获取ApplicationContext对象 */
 	public static ApplicationContext getApplicationContext() {
 		return applicationContext;
 	}
 
+	/** 根据beanname获取对象 */
 	public static Object getBean(String beanName) {
 		return applicationContext.getBean(beanName);
 	}
 
-	public static Map<String, Object> getBeansWithAnnotation(Class<? extends Annotation> annotationType) {
+	/** 根据类型获取对象 */
+	public static Object getBean(Class<?> clazz) {
+		return applicationContext.getBean(clazz);
+	}
 
+	/** 获取有指定注解的对象集合 */
+	public static Map<String, Object> getBeansWithAnnotation(Class<? extends Annotation> annotationType) {
 		return applicationContext.getBeansWithAnnotation(annotationType);
 	}
 
+	/** 获取有指定类型的对象集合 */
 	public static Map<String, ?> getBeansOfType(Class<?> type) {
 		return applicationContext.getBeansOfType(type);
 	}
--- springMVC/src/main/java/com/zyf/springMVC/customannotation/SysLoggerAspect.java
+++ springMVC/src/main/java/com/zyf/springMVC/customannotation/SysLoggerAspect.java
@@ -3,52 +3,61 @@ package com.zyf.springMVC.customannotation;
 import java.lang.reflect.Method;
 
 import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.annotation.After;
-import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 import org.aspectj.lang.reflect.MethodSignature;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 
+/** 注解@SysLogger的AOP */
 @Aspect
 @Component
 public class SysLoggerAspect {
 
 	private Logger logger;
 
+	/** 日志主题 */
 	private String logTheme;
-	
+
+	/** 切点 */
 	@Pointcut(value = "@annotation(SysLogger)")
 	public void pointCut() {
 	}
 
-	@Before("pointCut()")
-	public void before(JoinPoint point) {
-		getLogger().info(getLogTheme(point)+"...开始。");
+	/** 环绕通知 */
+	@Around("pointCut()")
+	public void around(ProceedingJoinPoint pj) {
+		Long start = System.currentTimeMillis();
+		getLogger(pj).info(getLogTheme(pj) + "...开始。");
+		try {
+			// 显式回调目标对象的原有方法
+			pj.proceed();
+		} catch (Throwable e) {
+			getLogger(pj).info(getLogTheme(pj) + "...异常：" + e.getMessage());
+			e.printStackTrace();
+		}
+		getLogger(pj).info(getLogTheme(pj) + "...结束。");
+		getLogger(pj).info(getLogTheme(pj) + "...耗时=" + (System.currentTimeMillis() - start) + "毫秒");
 	}
 
-	@After("pointCut()")
-	public void after(JoinPoint point) {
-		getLogger().info(getLogTheme(point)+"...结束。");
-	}
-	
-	@AfterThrowing("pointCut()")
-	public void afterThrowing(JoinPoint point) {
-		getLogger().info(getLogTheme(point)+"...异常结束。");
-	}
-	private Logger getLogger() {
+	/** 获取日志记录对象 */
+	private Logger getLogger(JoinPoint point) {
 		if (null != logger) {
 			return logger;
 		}
-		Logger logger = LoggerFactory.getLogger(this.getClass());
+		Logger logger = LoggerFactory.getLogger(point.getTarget().getClass());
 		return logger;
 	}
-	
+
+	/** 获取日志主题 */
 	private String getLogTheme(JoinPoint point) {
+		if (!StringUtils.isEmpty(logTheme)) {
+			return logTheme;
+		}
 		MethodSignature methodSignature = (MethodSignature) point.getSignature();
 		Method method = methodSignature.getMethod();
 		SysLogger annotation = method.getAnnotation(SysLogger.class);
