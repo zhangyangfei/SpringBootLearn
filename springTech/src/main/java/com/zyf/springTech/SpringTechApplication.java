ce0066ee6208aa0bce46c300f8a3f20345a55538
zhangyangfei <674557629@qq.com>
2019-11-18 20:39:17 +0800

    springTech技术

---
+++ springTech/src/main/java/com/zyf/springTech/SpringTechApplication.java
@@ -0,0 +1,14 @@
+package com.zyf.springTech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTechApplication.class, args);
+		System.out.println("hello springTech...");
+	}
+
+}
---
+++ springTech/src/test/java/com/zyf/springTech/SpringTechApplicationTests.java
@@ -0,0 +1,16 @@
+package com.zyf.springTech;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringTechApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
