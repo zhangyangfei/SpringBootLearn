d8f3d1ffc6114cef64edbb68dd80cdb0dbe8ad61
zhangyangfei <674557629@qq.com>
2019-11-20 23:04:03 +0800

    定时任务

---
+++ springTech/src/main/java/com/zyf/springTech/schedule/ScheduleConfig.java
@@ -0,0 +1,10 @@
+package com.zyf.springTech.schedule;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@Configuration
+@EnableScheduling // ※ 启用定时任务
+public class ScheduleConfig {
+
+}
