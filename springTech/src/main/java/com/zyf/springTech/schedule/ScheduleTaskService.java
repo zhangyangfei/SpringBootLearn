aa83ab16c22ee7141a5752b82a5cb23dc780d50e
zhangyangfei <674557629@qq.com>
2019-11-22 10:17:24 +0800

    spring定时任务的cron

--- springTech/src/main/java/com/zyf/springTech/schedule/ScheduleTaskService.java
+++ springTech/src/main/java/com/zyf/springTech/schedule/ScheduleTaskService.java
@@ -9,35 +9,43 @@ import org.springframework.stereotype.Component;
 public class ScheduleTaskService {
 
 	// 每5秒执行一次
-//	@Scheduled(fixedRate = 5000)
-	private void job1() {
+	@Scheduled(fixedRate = 5000)
+	public void job1() { // ※ 必须用public修饰
 		System.out.println("执行job1，时刻=" + System.currentTimeMillis());
 	}
 
 	// 每10秒执行一次
-//	@Scheduled(fixedRate = 10000)
+	@Scheduled(fixedRate = 10000)
 	@Async // 异步调用（异步线程池）
-	private void job2() {
+	public void job2() {
 		System.out.println("执行job1，时刻=" + System.currentTimeMillis());
 	}
 
-	// cron顺序：秒 分 时 天 月 周 年。年可以省略。通配符*表示任意值，?表示不指定值，避免日期和星期的冲突
+	// spring定时任务的cron顺序：秒 分 时 天 月 周 。
+	// ※ 不支持年。
+	// 通配符*表示任意值，?表示不指定值，避免日期和星期的冲突
+	// 第1列秒（0～59）  
+	// 第2列分钟0～59  
+	// 第3列小时0～23（0表示子夜）  
+	// 第4列日1～31  
+	// 第5列月1-12 或者 JAN-DEC  
+	// 第6列星期1-7 或者 SUN-SAT（1表示星期天） 
+	
 	// 每分钟的1秒执行
-//	@Scheduled(cron = "1 * * * * ?")
-	private void job3() {
+	@Scheduled(cron = "1 * * * * ?")
+	public void job3() {
 		System.out.println("执行job3，时刻=" + System.currentTimeMillis());
 	}
 
 	// 每天零点执行
-//	@Scheduled(cron = "0 0 0 * * ? ")
-	private void job4() {
+	@Scheduled(cron = "0 0 0 * * ? ")
+	public void job4() {
 		System.out.println("执行job4，时刻=" + System.currentTimeMillis());
 	}
 
-	// 通配符*表示任意值，?表示不指定值，L表示最后的，-表示时间段
-	// 2019年至2023年，每月最后的一个周五，23点30分，0秒，执行
-//	@Scheduled(cron = "0 30 23 ? * 6L 2019-2023")
-	private void job5() {
+	// 周五，23点30分，0秒，执行
+	@Scheduled(cron = "0 30 23 ? * 6")
+	public void job5() {
 		System.out.println("执行job5");
 	}
 }
