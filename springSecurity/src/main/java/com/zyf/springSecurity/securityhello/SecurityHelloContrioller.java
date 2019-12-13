b93d6f53686f7e60d39dc66f5f31624af12d066c
zhangyangfei <674557629@qq.com>
2019-12-13 14:13:49 +0800

    code format

--- springSecurity/src/main/java/com/zyf/springSecurity/securityhello/SecurityHelloContrioller.java
+++ springSecurity/src/main/java/com/zyf/springSecurity/securityhello/SecurityHelloContrioller.java
@@ -6,22 +6,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/securityhello")
 public class SecurityHelloContrioller {
+    /**
+     * 访问该路径，需要使用security自定生成的密码登录，用户名user。验证成功后进入sh1页面。
+     */
+    // http://localhost:8080/securityhello/sh1
+    @RequestMapping("/sh1")
+    public String sh1() {
+        return "securityhello/sh1";
+    }
 
-	/**
-	 * 访问该路径，需要使用security自定生成的密码登录，用户名user。验证成功后进入sh1页面。
-	 */
-	// http://localhost:8080/securityhello/sh1
-	@RequestMapping("/sh1")
-	public String sh1() {
-		return "securityhello/sh1";
-	}
-	
-	@RequestMapping("/sh2")
-	public String sh2() {
-		return "securityhello/sh1";
-	}
-	@RequestMapping("/sh3")
-	public String sh3() {
-		return "securityhello/sh1";
-	}
+    @RequestMapping("/sh2")
+    public String sh2() {
+        return "securityhello/sh1";
+    }
+
+    @RequestMapping("/sh3")
+    public String sh3() {
+        return "securityhello/sh1";
+    }
 }
