b79fbb6c54ce807189667f7e83ab204b818109b2
zhangyangfei <674557629@qq.com>
2020-12-17 23:07:48 +0800

    vue学习

---
+++ springboot-vue/src/main/java/com/zyf/springbootvue/SpringbootVueApplication.java
@@ -0,0 +1,14 @@
+package com.zyf.springbootvue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootVueApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootVueApplication.class, args);
+        System.out.println("springboot-vue启动完成！");
+    }
+
+}
---
+++ springboot-vue/src/test/java/com/zyf/springbootvue/SpringbootVueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zyf.springbootvue;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootVueApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
